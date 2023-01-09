97705aa572abf8c901f5424d551f89bba041ecc8
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-01-09 17:58:55 -0300

    v2.2.0 - post not allowing empty/not string

--- src/app.js
+++ src/app.js
@@ -9,41 +9,33 @@ const PORT = 5000;
 
 const users = [
   {
-    // username: "Canudin",
-    // avatar:
-    //   "https://img.freepik.com/premium-vector/cute-potato-vegetable-character-mascot-with-cool-gesture-isolated-cartoon-flat-style-design_574864-227.jpg?w=2000",
   },
 ];
 const tweets = [
-  // { username: "Canudin", tweet: "teste0" },
-  // { username: "Canudin", tweet: "teste1" },
-  // { username: "Canudin", tweet: "teste2" },
-  // { username: "Canudin", tweet: "teste3" },
-  // { username: "Canudin", tweet: "teste4" },
-  // { username: "Canudin", tweet: "teste5" },
-  // { username: "Canudin", tweet: "teste6" },
-  // { username: "Canudin", tweet: "teste7" },
-  // { username: "Canudin", tweet: "teste8" },
-  // { username: "Canudin", tweet: "teste9" },
-  // { username: "Canudin", tweet: "teste10" },
-  // { username: "Canudin", tweet: "teste11" },
-  // { username: "Canudin", tweet: "teste12" },
 ];
 
 server.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   const newUser = { username: username, avatar: avatar };
 
-  if (!username || !avatar) {
-    return res.status(400);
-  }
+  if (!username || !avatar) return res.sendStatus(400);
+
+  if (typeof username !== 'string' || typeof username !== 'string') return res.sendStatus(400);
+
   users.push(newUser);
   res.status(201).send("Ok!");
 });
 server.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-  console.log(req.body);
   const newTweet = { username: username, tweet: tweet };
+
+  if (!username || !tweet) {
+    return res.sendStatus(400);
+  }
+
+  if (typeof username !== 'string' || typeof tweet !== 'string') {
+    return res.sendStatus(400);
+  }
 
   if (!users.find((element) => element.username === newTweet.username)) {
     return res.status(401).send("Unauthorized");
@@ -55,10 +47,8 @@ server.post("/tweets", (req, res) => {
 server.get("/tweets", (req, res) => {
   const lastTweets = [...tweets.slice(-10)];
   const lastTweetsWithAvatar = [];
-  // console.log(lastTweets)
   lastTweets.map((item, index) => {
     const userInfo = users.find((element) => element.username === item.username);
-    // console.log(userInfo);
     const avatar = userInfo.avatar;
     const username = lastTweets[index].username;
     const tweet = lastTweets[index].tweet;
