7e0fff462f69e8a98f1f93b2afa923f43a0e407d
Giovanni Marcatto <gzmarcatto@gmail.com>
2023-01-09 13:09:19 -0300

    v2.0 - .get and .post implemented and working properly

--- src/app.js
+++ src/app.js
@@ -3,24 +3,44 @@ import express from "express";
 
 const server = express();
 server.use(cors());
+server.use(express.json())
 
 const PORT = 5000;
 
-const user = [];
-const tweet = [];
+const users = [];
+const tweets = [];
 
 server.post("/sign-up", (req, res) => {
-  res.send(user);
-});
-server.get("/sign-up", (req, res) => {
-  console.log(req);
-  res.send(user);
+  const {username, avatar} = req.body
+  const newUser = {username: username, avatar: avatar}
+
+
+  users.push(newUser)
+  res.send("Ok!");
 });
 server.post("/tweets", (req, res) => {
-  res.send(user);
+  const {username, tweet} = req.body
+  const newTweet = {username: username, tweet: tweet}
+
+  if (!users.find(element => element === newTweet.username)){
+    return res.status(401).send("Unauthorized")
+  }
+  
+  tweets.push(newTweet)
+  res.send("Ok!");
 });
 server.get("/tweets", (req, res) => {
-  res.send(user);
+  const lastTweets = []
+  for (let i=0; i<10; i++){
+    if (!tweets[i]) return
+    const username = tweets[i].username
+    const tweet = tweets[i].tweet
+    const userInfo = users.find(element => element.username === tweets[i].username)
+    const avatar = userInfo.avatar
+    lastTweets.push({username: username, avatar: avatar, tweet:tweet})
+  }
+
+  res.send(lastTweets.reverse());
 });
 
 server.listen(PORT, () => {
